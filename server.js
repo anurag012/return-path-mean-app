94eb1a463634da2811c2069ca9b205a93f2ef1f6
Anurag <anuraggarg.57@gmail.com>
2016-08-14 20:26:09 -0500

    Final Commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var database = require('./config/database.config');
 mongoose.connect(database.url);
 app.use(bodyParser.json());
 
-app.use('/api/v1',express.static(__dirname+"/public"));
+app.use(express.static(__dirname+"/public"));
 require('./models/user.model');
 require('./routes')(app);
 
