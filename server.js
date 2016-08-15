4c5687a61b9f41a7a7593ad74613f0d0fea9ac93
Anurag <anuraggarg.57@gmail.com>
2016-08-14 21:15:28 -0500

    Final Commit

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ require('./models/user.model');
 require('./routes')(app);
 
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 console.log("Listening on port 3000");
 
